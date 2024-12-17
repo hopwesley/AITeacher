821b609b527ec761c0e89243cb6561d724f05f4f
basdevelop <hyperorchid@outlook.com>
2024-12-17 19:25:53 +0800

    bug fixed

--- chapter6/6.4/6.4.1/main.go
+++ chapter6/6.4/6.4.1/main.go
@@ -11,64 +11,71 @@ import (
 var matchQueue []string
 var queueMutex sync.Mutex
 
+// 升级 HTTP 连接为 WebSocket 连接
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
 func matchHandler(w http.ResponseWriter, r *http.Request) {
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        http.Error(w, "WebSocket 升级失败", http.StatusInternalServerError)
-        return
-    }
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "WebSocket 升级失败", http.StatusInternalServerError)
+		return
+	}
 
-    defer conn.Close()
+	defer conn.Close()
 
-    var player string
+	var player string
 
-    for {
-        _, msg, err := conn.ReadMessage()
-        if err != nil {
-            return
-        }
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			return
+		}
 
-        // 解析消息，假设格式为 {"type": "matchRequest", "player": "Player1"}
-        var data struct {
-            Type   string `json:"type"`
-            Player string `json:"player"`
-        }
-        json.Unmarshal(msg, &data)
+		// 解析消息，假设格式为 {"type": "matchRequest", "player": "Player1"}
+		var data struct {
+			Type   string `json:"type"`
+			Player string `json:"player"`
+		}
+		json.Unmarshal(msg, &data)
 
-        if data.Type == "matchRequest" {
-            player = data.Player
-            queueMutex.Lock()
-            matchQueue = append(matchQueue, player)
-            queueMutex.Unlock()
+		if data.Type == "matchRequest" {
+			player = data.Player
+			queueMutex.Lock()
+			matchQueue = append(matchQueue, player)
+			queueMutex.Unlock()
 
-            // 检查队列中是否有两个玩家
-            queueMutex.Lock()
-            if len(matchQueue) >= 2 {
-                opponent := matchQueue[0]
-                if opponent == player {
-                    opponent = matchQueue[1]
-                }
-                matchQueue = matchQueue[2:]
-                queueMutex.Unlock()
+			// 检查队列中是否有两个玩家
+			queueMutex.Lock()
+			if len(matchQueue) >= 2 {
+				opponent := matchQueue[0]
+				if opponent == player {
+					opponent = matchQueue[1]
+				}
+				matchQueue = matchQueue[2:]
+				queueMutex.Unlock()
 
-                conn.WriteJSON(map[string]string{
-                    "type":     "matchSuccess",
-                    "opponent": opponent,
-                })
-                return
-            }
-            queueMutex.Unlock()
-        }
-    }
+				conn.WriteJSON(map[string]string{
+					"type":     "matchSuccess",
+					"opponent": opponent,
+				})
+				return
+			}
+			queueMutex.Unlock()
+		}
+	}
 }
 
 func main() {
 
-  // 提供静态文件服务
-    fs := http.FileServer(http.Dir("."))
-    http.Handle("/", fs)
+	// 提供静态文件服务
+	fs := http.FileServer(http.Dir("."))
+	http.Handle("/", fs)
 
-    http.HandleFunc("/match", matchHandler)
+	http.HandleFunc("/match", matchHandler)
 
 	fmt.Println("服务器启动，访问地址：http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
